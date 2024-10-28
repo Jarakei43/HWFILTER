fdff5aa3519ce07305409edddc4772d6d6648d3e
Jarakei43 <134204371+Jarakei43@users.noreply.github.com>
2024-10-28 18:02:51 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+//N1
+// const num = [1, 2, 3, 4, 5, 6];
+// const filteredNum = num.filter((item) => {
+//   return item % 2 === 0;
+// });
+// console.log(filteredNum);
+
+// N2
+// const names = ["кот", "собака", "тигр", "лев"];
+// const filtredNames = names.filter((item) => {
+//   return item.length > 3;
+// });
+// console.log(filtredNames);
+
+// N3
+// const num2 = [5, 12, 8, 20, 3]
+// const filNum = num2.filter((item) => {
+//     return item > 10
+// })
+// console.log(filNum);
+
+// N4
+// const str = ['апельсин', 'яблоко', 'арбуз', 'банан']
+// const startWith = str.filter((item) => {
+//     return item.startsWith("а")
+// })
+// console.log(startWith);
+
+// N5
+// const user = [
+//   { name: "Alice", age: 17 },
+//   { name: "Hannah", age: 30 },
+//   { name: "Fiona", age: 5 },
+//   { name: "George", age: 35 },
+//   { name: "Bob", age: 13 },
+//   { name: "Alice", age: 31 },
+//   { name: "Charlie", age: 16 },
+//   { name: "George", age: 31 },
+//   { name: "Hannah", age: 21 },
+//   { name: "George", age: 14 },
+// ];
+// const minors = user.filter((item) => {
+//   return item.age > 18;
+// });
+// console.log(minors);
